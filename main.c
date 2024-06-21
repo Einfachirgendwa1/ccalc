ddacf96e5f4016cc29e7b87dd030b0057e5534b1
Einfachirgendwa1 <faris@karge.berlin>
2024-06-21 16:25:12 +0200

    Bug gefixt, Klammerrechnung funktioniert jetzt richtig, Debugprinting hinzugefügt

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
@@ -6,6 +7,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEBUG false
+
+#define TERM printf("\033[0m")
+
+#define RED printf("\033[31m")
+#define YELLOW printf("\033[33m")
+#define GREEN printf("\033[32m")
+#define CYAN printf("\033[36m")
+
+#define DBGSTART                                                                  \
+    YELLOW;                                                                       \
+    switch (evalcallstack) {                                                      \
+        case 0:                                                                   \
+            printf("%d in %s main\t: DEBUG ", __LINE__, __FILE__);                \
+            break;                                                                \
+        case 1:                                                                   \
+            printf("%d in %s eval\t: DEBUG ", __LINE__, __FILE__);                \
+            break;                                                                \
+        default:                                                                  \
+            printf("%d in %s %d\t\t: DEBUG ", __LINE__, __FILE__, evalcallstack); \
+            break;                                                                \
+    }
+#define COLOREND \
+    TERM;        \
+    printf("\n")
+#define DEBUGPRINT(...)      \
+    if (DEBUG) {             \
+        DBGSTART;            \
+        printf(__VA_ARGS__); \
+        COLOREND;            \
+    }
+
+#define RETURNERROR(x)                                  \
+    DEBUGPRINT("Fehler aufgetreten, returne Error..."); \
+    return err_res(x)
+
 #define EPSILON 1e-9
 
 #define FEQ(left, right) fabs(left - right) < EPSILON
@@ -13,6 +50,8 @@
 #pragma clang diagnostic warning "-Weverything"
 #pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
 #pragma clang diagnostic ignored "-Wpadded"
+
+static int evalcallstack = 0;
 
 typedef struct Result {
     enum {
@@ -34,26 +73,33 @@ int main(void) {
     char buf[32];
     Result *result;
 
+    DEBUGPRINT("Debugprinting ist aktiviert.")
     while (1) {
+        CYAN;
         printf(" > ");
         if (fgets(buf, sizeof(buf), stdin) == 0) {
             printf("\n");
             return 0;
         }
+        TERM;
         buf[strlen(buf) - 1] = '\0';
         result = eval(buf, 0, (uint32_t)strlen(buf));
 
-        if (result->type == ERROR) {
-            printf("%s\n", result->data.msg);
+        if (result->type == DOUBLE) {
+            GREEN;
+            printf("%s = %f", buf, result->data.dval);
         } else {
-            printf("%s = %f\n", buf, result->data.dval);
+            RED;
+            printf("%s", result->data.msg);
         }
+        COLOREND;
         free(result);
     }
 }
 
 static Result *val_res(double val) {
     Result *new = malloc(sizeof(Result));
+    assert(1 + 1 == 2);
     new->type = DOUBLE;
     new->data.dval = val;
     return new;
@@ -95,36 +141,56 @@ static Result *eval(char buf[32], uint32_t left, uint32_t right) {
                 }
 
                 if (c == '*' || c == '/') {
-                    return err_res("Rechenzeichen ohne Zahl davor.");
+                    DEBUGPRINT("%c", c)
+                    RETURNERROR("Rechenzeichen ohne Zahl davor.");
                 }
             }
             if (c == '(') {
                 uint32_t start = idx + 1;
                 Result *res;
+                DEBUGPRINT("Klammer gefunden.")
                 while (c != ')') {
                     c = buf[++idx];
                     if (c == '\0') {
-                        return err_res("Klammer wurde nicht korrekt geschlossen");
+                        RETURNERROR("Klammer wurde nicht korrekt geschlossen");
                     }
                 }
+                evalcallstack += 1;
                 res = eval(buf, start, idx);
+                evalcallstack -= 1;
                 if (res->type == ERROR) {
                     return res;
                 }
-                if (!left_value.exists) {
-                    return val_res(left_value.dval * res->data.dval);
+                DEBUGPRINT("Klammer Evaluation returnt %f", res->data.dval)
+                if (left_value.exists) {
+                    double data = res->data.dval;
+                    free(res);
+                    DEBUGPRINT("Implizieter Klammer return.")
+                    return val_res(left_value.dval * data);
                 } else {
                     left_value.dval = res->data.dval;
+                    left_value.exists = true;
                     free(res);
                 }
                 continue;
             }
+            evalcallstack += 1;
             right_res = eval(buf, idx + 1, right);
+            evalcallstack -= 1;
             if (right_res->type == ERROR) {
                 return right_res;
             }
+            if (DEBUG) {
+                DBGSTART
+                for (uint32_t didx = idx + 1; didx <= right; didx++) {
+                    printf("%c", buf[didx]);
+                }
+                printf(" returnt %f", right_res->data.dval);
+                COLOREND;
+            }
             right_value = right_res->data.dval;
             free(right_res);
+            DEBUGPRINT("%f %c %f", right_value, c, left_value.dval)
             switch (c) {
                 case '+':
                     return val_res(left_value.dval + right_value);
@@ -134,16 +200,16 @@ static Result *eval(char buf[32], uint32_t left, uint32_t right) {
                     return val_res(left_value.dval * right_value);
                 case '/':
                     if (FEQ(right_value, 0.0)) {
-                        return err_res("Division durch 0");
+                        RETURNERROR("Division durch 0");
                     }
                     return val_res(left_value.dval / right_value);
                 default:
-                    return err_res("Unerwartes Rechenzeichen gefunden.");
+                    RETURNERROR("Unerwartes Rechenzeichen gefunden.");
             }
         }
     }
     if (!left_value.exists) {
-        return err_res("Expression erwartet, aber nichts gefunden.");
+        RETURNERROR("Expression erwartet, aber nichts gefunden.");
     }
     return val_res(left_value.dval);
 }
